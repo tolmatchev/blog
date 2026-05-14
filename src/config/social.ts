93c0a0f8d1cfa8a32a0f34c4b0232b09a7eaa60b
Толмачев Никита Александрович <forblachok@yandex.ru>
2026-05-14 16:01:24 +0300

    add telegram icon

--- src/config/social.ts
+++ src/config/social.ts
@@ -1,43 +1,35 @@
 import type { SocialLink } from "../types";
 
 export const SOCIALS: SocialLink[] = [
-    {
-        name: "Github",
-        href: "https://github.com/shannon",
-        linkTitle: `Follow Claude Shannon on Github`,
-        isActive: true,
-    },
-    {
-        name: "Mail",
-        href: "mailto:shannon@bell-labs.com",
-        linkTitle: `Send an email to Claude`,
-        isActive: true,
-    },
-    {
-        name: "Google Scholar",
-        href: "https://scholar.google.com/citations?user=shannon",
-        linkTitle: `Claude Shannon on Google Scholar`,
-        isActive: true,
-    },
-    {
-        name: "ORCID",
-        href: "https://orcid.org/0000-0002-1825-0097",
-        linkTitle: `Claude Shannon on ORCID`,
-        isActive: true,
-    },
-    {
-        name: "LinkedIn",
-        href: "https://www.linkedin.com/in/claude-shannon-123456789/",
-        linkTitle: `Claude Shannon on LinkedIn`,
-        isActive: true, // Assuming Claude doesn't have a LinkedIn profile
-    },
+  {
+    name: "Github",
+    href: "https://github.com/Guzime",
+    linkTitle: `Follow Tolmachev Nikita on Github`,
+    isActive: true,
+  },
+  {
+    name: "Mail",
+    href: "mailto:forblachok@yandex.ru",
+    linkTitle: `Send an email to Tolmachev Nikita`,
+    isActive: true,
+  },
+  {
+    name: "LinkedIn",
+    href: "https://www.linkedin.com/in/claude-shannon-123456789/",
+    linkTitle: `Claude Shannon on LinkedIn`,
+    isActive: false,
+  },
+  {
+    name: "Telegram",
+    href: "https://t.me/tolmachevski",
+    linkTitle: `Tolmachev Nikita in Telegram`,
+    isActive: true, 
+  },
 ];
 
 export const SOCIAL_ICONS: Record<string, string> = {
-    Github: "Github",
-    Mail: "Mail",
-    Linkedin: "LinkedIn",
-    "Google Scholar": "GoogleScholar",
-    ORCID: "ORCID",
-    RSS: "RSS",
+  Github: "Github",
+  Mail: "Mail",
+  Linkedin: "LinkedIn",
+  Telegram: "Telegram",
 };
